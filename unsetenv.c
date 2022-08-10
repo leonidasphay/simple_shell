c6d61df0653560634512ed450655d3eda7841ccb
Favour <favourkolawole21@gmail.com>
2022-08-10 21:49:08 +0100

    corrections

--- unsetenv.c
+++ unsetenv.c
@@ -1,4 +1,5 @@
 #include "builtins.h"
+#include "general.h"
 
 /**
 * bin_unsetenv - unsets the environment variable
@@ -8,18 +9,18 @@
 
 int bin_unsetenv(general_t *info)
 {
-char **args = info->tokens + 1;
+	char **args = info->tokens + 1;
 
-if (*args)
-{
-while (*args)
-del_dict_node(&info->env, *args++);
-info->status = EXIT_SUCCESS;
-}
-else
-{
-perrorl("Too few arguments.", *info->tokens, NULL);
-info->status = EXIT_FAILURE;
-}
-return (info->status);
+	if (*args)
+	{
+		while (*args)
+			del_dict_node(&info->env, *args++);
+		info->status = EXIT_SUCCESS;
+	}
+	else
+	{
+		perrorl("Too few arguments.", *info->tokens, NULL);
+		info->status = EXIT_FAILURE;
+	}
+	return (info->status);
 }
